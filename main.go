a7458e13f02f29b69e8747e4b1cb32a29535ce90
Alexander Konyukov <scor2k@gmail.com>
2021-12-06 22:32:45 -0500

    feat: init cli app

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/urfave/cli/v2"
+	"os"
+)
+import "github.com/op/go-logging"
+
+var log = logging.MustGetLogger("uptimestats")
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} ▶ %{level:.8s} %{shortfunc} %{color:reset} %{message}`,
+)
+
+func main() {
+	appLogs := logging.NewLogBackend(os.Stderr, "", 0)
+	appLogsFormatter := logging.NewBackendFormatter(appLogs, format)
+	appLogsLeveled := logging.AddModuleLevel(appLogsFormatter)
+	appLogsLeveled.SetLevel(logging.INFO, "")
+	logging.SetBackend(appLogsLeveled)
+
+	app := &cli.App{
+		Name:     "go-mesh-mon",
+		HelpName: "go-mesh-mon",
+		Usage:    "Mesh Monitoring app for small projects",
+		Flags:    []cli.Flag{},
+		Commands: []*cli.Command{
+			{
+				Name:  "serve",
+				Usage: "start API server and begin work",
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:  "debug",
+						Value: false,
+						Usage: "Enable debug logging",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					if c.Bool("debug") {
+						appLogsLeveled.SetLevel(logging.DEBUG, "")
+					}
+
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
